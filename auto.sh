a34bb89bb5dfaf333ebe1948af10a0c4018e87a4
jeffrey.yen2 <jeffrey.yen2@gmail.com>
2016-12-11 15:59:54 +0800

    15-59-54

---
+++ auto.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+cd /c/Users/302-TCFST/test-iii/pic
+
+ls
+while true
+do
+  echo 'test'
+  fc="$(ls | wc -l)"
+  if [ $fc -gt 0 ];
+    then
+	  echo 'test1'
+      FILES=/c/Users/302-TCFST/test-iii/pic/*
+	  echo 'test2'
+      for f in $FILES
+      do
+        /mingw64/bin/git add $f
+        /mingw64/bin/git commit -m "$(date +%H-%M-%S)"
+        /mingw64/bin/git remote add origin git@github.com/JeffreyYen/iii.git
+        /mingw64/bin/git push -u origin master
+        /mingw64/bin/git remote remove origin
+      done
+  fi;
+  rm -rf /c/Users/302-TCFST/test-iii/pic/*
+  sleep 3
+done
